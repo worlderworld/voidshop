8a9845fb43e3993ec23d4f03f847849c9d74b222
worlderworld <slightwonderworld@gmail.com>
2024-12-19 08:29:26 +0530

    Reapply "0"
    This reverts commit 8d8509859a1cec308440025fcc33db4bd1121241.

---
+++ src/Products.js
@@ -0,0 +1,202 @@
+export const products = {
+  category: [
+    {
+      category: 'T Shirt',
+      data: [
+        {
+          name: 't-shirt 1',
+          price: 300,
+          gender: 'male',
+          image: require('./Images/tshirt1.jpg'),
+        },
+        {
+          name: 't-shirt 2',
+          price: 349,
+          gender: 'male',
+          image: require('./Images/tshirt2.jpeg'),
+        },
+        {
+          name: 't-shirt 3',
+          price: 200,
+          gender: 'male',
+          image: require('./Images/tshirt3.webp'),
+        },
+        {
+          name: 't-shirt 4',
+          price: 299,
+          gender: 'male',
+          image: require('./Images/tshirt4.webp'),
+        },
+        {
+          name: 't-shirt 5',
+          price: 399,
+          gender: 'male',
+          image: require('./Images/tshirt5.webp'),
+        },
+      ],
+    },
+    {
+      category: 'Jeans',
+      data: [
+        {
+          name: 'Jeans 1',
+          price: 500,
+          gender: 'male',
+          image: require('./Images/jeans1.webp'),
+        },
+        {
+          name: 'Jeans 2',
+          price: 499,
+          gender: 'male',
+          image: require('./Images/jeans2.webp'),
+        },
+        {
+          name: 'Jeans 3',
+          price: 599,
+          gender: 'male',
+          image: require('./Images/jeans3.jpeg'),
+        },
+        {
+          name: 'Jeans 4',
+          price: 699,
+          gender: 'male',
+          image: require('./Images/jeans4.webp'),
+        },
+      ],
+    },
+    {
+      category: 'Shoes',
+      data: [
+        {
+          name: 'Shoes 1',
+          price: 1100,
+          gender: 'male',
+          image: require('./Images/shoes1.jpeg'),
+        },
+        {
+          name: 'Shoes 2',
+          price: 680,
+          gender: 'male',
+          image: require('./Images/shoes2.jpeg'),
+        },
+        {
+          name: 'Shoes 3',
+          price: 599,
+          gender: 'male',
+          image: require('./Images/shoes3.jpeg'),
+        },
+        {
+          name: 'Shoes 4',
+          price: 800,
+          gender: 'male',
+          image: require('./Images/shoes4.jpeg'),
+        },
+        {
+          name: 'Shoes 5',
+          price: 1000,
+          gender: 'male',
+          image: require('./Images/shoes5.webp'),
+        },
+      ],
+    },
+    {
+      category: 'Jacket',
+      data: [
+        {
+          name: 'Jacket 1',
+          price: 1300,
+          gender: 'male',
+          image: require('./Images/jacket1.webp'),
+        },
+        {
+          name: 'Jacket 2',
+          price: 999,
+          gender: 'male',
+          image: require('./Images/jacket2.webp'),
+        },
+        {
+          name: 'Jacket 3',
+          price: 650,
+          gender: 'male',
+          image: require('./Images/jacket3.webp'),
+        },
+        {
+          name: 'Jacket 4',
+          price: 800,
+          gender: 'male',
+          image: require('./Images/jacket4.webp'),
+        },
+      ],
+    },
+    {
+      category: 'Slipper',
+      data: [
+        {
+          name: 'Slippers 1',
+          price: 250,
+          gender: 'male',
+          image: require('./Images/slipper1.webp'),
+        },
+        {
+          name: 'Slipper 2',
+          price: 450,
+          gender: 'male',
+          image: require('./Images/slipper2.webp'),
+        },
+        {
+          name: 'Slipper 3',
+          price: 400,
+          gender: 'male',
+          image: require('./Images/slipper3.jpeg'),
+        },
+        {
+          name: 'Slipper 4',
+          price: 240,
+          gender: 'male',
+          image: require('./Images/slipper4.jpeg'),
+        },
+        {
+          name: 'Slipper 5',
+          price: 399,
+          gender: 'male',
+          image: require('./Images/slipper5.jpeg'),
+        },
+      ],
+    },
+    {
+      category: 'Trousers',
+      data: [
+        {
+          name: 'Trousers 1',
+          price: 250,
+          gender: 'male',
+          image: require('./Images/trouser1.jpg'),
+        },
+        {
+          name: 'Trousers 2',
+          price: 450,
+          gender: 'male',
+          image: require('./Images/trouser2.webp'),
+        },
+        {
+          name: 'Trousers 3',
+          price: 400,
+          gender: 'male',
+          image: require('./Images/trouser3.webp'),
+        },
+        {
+          name: 'Trousers 4',
+          price: 240,
+          gender: 'male',
+          image: require('./Images/trouser4.jpeg'),
+        },
+        {
+          name: 'Trousers 5',
+          price: 399,
+          gender: 'male',
+          image: require('./Images/trouser5.jpeg'),
+        },
+      ],
+    },
+  ],
+};
