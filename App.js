d3d55dc9625d41fa954087b6ba92bf92c354bac6
worlderworld <slightwonderworld@gmail.com>
2024-12-19 07:56:57 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import AppNavigator from './src/AppNavigator'
+import { Provider } from 'react-redux'
+import MainContainer from './src/MainContainer'
+import  {store}  from './src/redux/store/Store'
 
 const App = () => {
   return (
-<AppNavigator/>
+    <Provider store={store}>
+      <MainContainer/>
+    </Provider>
   )
 }
 
